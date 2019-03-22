6b6afbeedb975c90d76883f73e004a2ff9b69c18
MetalJester <acjswanson@gmail.com>
2019-03-21 23:47:20 -0700

    added functions to take in customer order, check quantity wanted and respond based on current inventory

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,43 +15,80 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     // console.log("I'm connected to the database");
-    readProducts();
-    start();
+    displayInventory()
 });
 
 //function to display table of products
-function readProducts() {
-    console.log("These products are availabe for purchase.\n");
+
+
+function displayInventory(){
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
+        console.log("These products are availabe for purchase.\n");
         // Log all results of the SELECT statement
         console.table(res);
-        connection.end();
+        // connection.end();
+        start();
     });
-};
+}
 
 //function to prompt customer to make a purchase
 function start() {
-    inquirer
-        .prompt({
+    inquirer.prompt([
+        {
             name: "item",
             type: "input",
-            message: "Type the ID number of the item you would like to purchase."
+            message: "Enter the Item ID number of the item you would like to purchase:"
         },
-            {
-                name: "quantity",
-                type: "input",
-                message: "Enter quantity of this ite you would like to purchase."
-            })
-    //   .then(function(answer) {
-    //     // based on their answer, either call the bid or the post functions
-    //     if (answer.item === "POST") {
-    //       postAuction();
-    //     }
-    //     else if(answer.item === "BID") {
-    //       bidAuction();
-    //     } else{
-    //       connection.end();
-    //     }
-    //   });
-};
+        {
+            name: "quantity",
+            type: "input",
+            message: "Enter quantity of this item you would like to purchase:"
+        }
+    ]).then(function (answer) {
+        var customerItem = answer.item;
+        var customerQty = answer.quantity;
+        customerOrder(customerItem, customerQty);
+    })
+}
+
+function customerOrder(itemID, qtyNeeded) {
+    connection.query("SELECT * FROM products WHERE item_id = " + itemID, function (err, res) {
+        if (err) throw err;
+        if (qtyNeeded <= res[0].stock_quantity) {
+            console.log("Thank you for your purchase. Your total is $0.00")
+            displayInventory();
+        }
+        else {
+            console.log("Insufficient quantity; cannot complete your order.");
+            displayInventory();
+        };
+        
+    });
+}
+
+
+
+
+
+
+
+// Prompt for quantitye
+// inquirer
+//         .prompt({
+//             name: "quantity",
+//             type: "input",
+//             message: "Enter quantity of this item you would like to purchase:"
+//         })
+//         .then(function (answer) {
+//             // based on their answer, either call the bid or the post functions
+//             if (answer.quantitu === "POST") {
+//                 postAuction();
+//             }
+//             else if (answer.quantity === "BID") {
+//                 bidAuction();
+//             } else {
+//                 connection.end();
+//             }
+
+//         });
